c5ca9b167b9d3375000d185b1e6c90df078bf244
lukewhyte <lukeawhyte@gmail.com>
2018-08-23 12:01:16 -0500

    Added pym support

--- src/main.js
+++ src/main.js
@@ -1,4 +1,6 @@
 import Vue from 'vue'
+import pym from 'pym.js'
+
 import titleMixin from './mixins/titleMixin'
 
 import Index from './components/Index/Index.vue'
@@ -7,6 +9,21 @@ Vue.mixin(titleMixin)
 
 Vue.config.productionTip = false
 
-new Vue({
-  render: h => h(Index)
-}).$mount('#app')
+let pymChild;
+
+const renderVue = () => new Vue({
+  render: h => h(Index),
+  mounted: () => {
+    if (pymChild) {
+        pymChild.sendHeight();
+    }
+  },
+}).$mount('#app');
+
+function load () {
+    pymChild = new pym.Child({
+        renderCallback: renderVue
+    });
+}
+
+window.onload = load;
