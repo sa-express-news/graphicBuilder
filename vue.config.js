37d4bdcab74433a3a05e8454f11e03482fd62ffa
Luke Whyte <lukeawhyte@gmail.com>
2018-10-11 17:03:17 -0500

    Added build script configuration, deployment configuration, data.json default and svg component handling

---
+++ vue.config.js
@@ -0,0 +1,47 @@
+const CompressionWebpackPlugin = require('compression-webpack-plugin');
+const path  = require('path');
+
+const isProduction = () => process.env.NODE_ENV === 'production';
+
+const setSvgRule = config => {
+    const svgRule = config.module.rule('svg');
+    svgRule.uses.clear();
+    svgRule
+        .use('vue-svg-loader')
+        .loader('vue-svg-loader')
+        .tap(options => {
+            return Object.assign({}, options, { 
+                svgo: {
+                    plugins: [
+                        {removeDoctype: true},
+                        {removeComments: true},
+                        {cleanupIDs: false}
+                    ]
+                }
+            });
+        });
+    return svgRule;
+};
+
+const setGzipRule = config => {
+    const gzipRule = config.plugin('gzip');
+    gzipRule.use(new CompressionWebpackPlugin({
+        filename: '[file]',
+        algorithm: 'gzip',
+        test: /\.js$|\.css$|\.html$|\.eot?.+$|\.ttf?.+$|\.woff?.+$|\.svg?.+$/,
+        threshold: 10240,
+        minRatio: 0.8,
+    }));
+    return gzipRule;
+};
+
+module.exports = {
+    chainWebpack: config => {
+        const svgRule = setSvgRule(config);
+
+        if (isProduction()) {
+            const gzipRule = setGzipRule(config);
+        }
+    },
+    baseUrl: './',
+};
